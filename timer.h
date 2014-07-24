ea102fc04ae565eaf1819b6a187cfe1460b3e631
Thomas Lauer <thomaslauer01@gmail.com>
2014-07-23 19:55:45 -0500

    Added createTimer methods

--- timer.h
+++ timer.h
@@ -1,10 +1,23 @@
 #ifndef TIMER
 #define TIMER
 
-typedef struct {
+typedef struct 
+{
 	TTimers timer;
-	long lastTime;
-	long targetTime;
+	int lastTime;
+	int targetTime;
 }Timer;
+
+void createTimer(Timer &t, TTimers timer = T2, int target)
+{
+	t.timer = timer;
+	t.lastTime = time1[timer];
+	t.targetTime = target;
+}
+
+void createTimer(Timer &t, TTimers timer = T2)
+{
+	createTimer(t, timer, 0);
+}
 
 #endif
