6555be986b85276824ceeb98d48d953cc031f74e
Thomas Lauer <thomaslauer01@gmail.com>
2014-07-23 20:16:35 -0500

    commented the motor control file

--- motor control.h
+++ motor control.h
@@ -3,35 +3,37 @@
 
 #include "math utils.h"
 
-typedef enum {
+typedef enum // this enum is used to denote different movement directions, only for the simple movement below
+{
 	FORWARD, BACKWARD, RIGHT, LEFT
-} MoveDirections;
+} SimpleMoveDirections;
 
-void move(MoveDirections direction, int speed, int duration)
+void move(SimpleMoveDirections direction, int speed, int duration)
 {
 	//capNumber(&speed, -100, 100);
-	switch(direction){
-	case FORWARD:
+	switch(direction) // decides what combination of motors to move
+	{
+	case FORWARD: // forward
 		motor[motorD] = speed;
 		motor[motorE] = speed;
 		break;
-	case BACKWARD:
+	case BACKWARD: // backward
 		motor[motorD] = -speed;
 		motor[motorE] = -speed;
 		break;
-	case RIGHT:
+	case RIGHT: // right
 		motor[motorD] = -speed;
 		motor[motorE] = speed;
 		break;
-	case LEFT:
+	case LEFT: // take a guess
 		motor[motorD] = speed;
 		motor[motorE] = -speed;
 		break;
 	default:
 		break;
 	}
-	wait1Msec(duration);
-	motor[motorD] = 0;
+	wait1Msec(duration);	// waits the duration in msecs, should probably use the timer file for this
+	motor[motorD] = 0; 		// kills the motors
 	motor[motorE] = 0;
 }
 #endif
