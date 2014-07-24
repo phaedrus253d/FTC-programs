7eea747b341956b42e3bd37098a1124d9b8efc9c
Thomas Lauer <thomaslauer01@gmail.com>
2014-07-23 20:12:22 -0500

    Added a lot of timer stuff

--- timer.h
+++ timer.h
@@ -1,23 +1,54 @@
 #ifndef TIMER
 #define TIMER
 
-typedef struct 
+typedef struct // The basic timer struct
 {
-	TTimers timer;
-	int lastTime;
-	int targetTime;
+	TTimers timer; 	// The hardware timer used
+	int lastTime;		// last time the timer was read
+	int targetTime;	// the target of the timer
 }Timer;
-
-void createTimer(Timer &t, TTimers timer = T2, int target)
-{
-	t.timer = timer;
-	t.lastTime = time1[timer];
-	t.targetTime = target;
-}
 
 void createTimer(Timer &t, TTimers timer = T2)
 {
-	createTimer(t, timer, 0);
+	t.timer = timer;
+	t.lastTime = time1[timer];
+	t.targetTime = 0;
+}
+
+void clearTimer(Timer &t)				// clears the timer
+{
+	t.lastTime = time1[t.timer];	// all we do here is set the last time read to the current time
+}
+
+void setTimerTarget(Timer &t, int msec)	// sets the target of the timer
+{
+	t.targetTime = time1[t.timer] + msec;	// sets the target to the current time + target
+	t.lastTime = time1[t.timer];						// sets last time to current time, so difference is the target
+}
+
+void addToTimerTarget(Timer &t, int msecToAdd)	// adds a number of msecs to the target
+{
+	t.targetTime += msecToAdd;
+}
+
+void subtractFromTimerTarget(Timer &t, int msecToSub) // removes a number of msecs from target
+{
+	t.targetTime -= msecToSub;
+}
+
+int getTimeFromTimer(Timer &t) // gets the difference in the current time and the last time read
+{
+	return time1[t.timer] - t.lastTime;
+}
+
+bool isTimerPastTarget(Timer &t) // checks to see if the timer is past the target
+{
+	return t.targetTime >= time1[t.timer];
+}
+
+bool isTimerBeforeTarget(Timer &t) // checks to see if the timer is before the target
+{
+	return t.targetTime < time1[t.timer];
 }
 
 #endif
