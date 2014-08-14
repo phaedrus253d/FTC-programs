056408711737856c26e8fc3adf8e5bf9b2ff518e
Thomas Lauer <thomaslauer01@gmail.com>
2014-08-14 12:06:20 -0500

    Making a send cmd method for smux

--- libs/smux utils.h
+++ libs/smux utils.h
@@ -1,17 +1,24 @@
 #ifndef SMUX
 #define SMUX
 
-void smuxHalt(tSensors smux)
-{
-	ubyte sendMsg[4];
+#define SMUX_CMD_HALT 0x00
+#define SMUX_CMD_AUTODETECT 0x01
+#define SMUX_CMD_RUN 0x02
 
-	sendMsg[0] = 3;		//size of i2c message
-	sendMsg[1] = 0x10;	//address of smux
-	sendMsg[2] = 0x20;	//command register
-	sendMsg[3] = 0;		//Command to send (HALT)
+void smuxSendCmd(tSensors smux, char command)
+{
+	char sendMsg[4];
+
+	sendMsg[0] = 3;					//size of i2c message
+	sendMsg[1] = 0x10;			//address of smux
+	sendMsg[2] = 0x20;			//command register
+	sendMsg[3] = command;		//Command to send
 
 	sendI2CMsg(smux, sendMsg, 0);
-	wait1Msec(50);
+	if(command == SMUX_CMD_AUTODETECT)
+		wait1Msec(500);
+	if(command == SMUX_CMD_HALT)
+		wait1Msec(50);
 }
 
 #endif
